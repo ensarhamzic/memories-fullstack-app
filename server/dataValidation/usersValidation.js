2f5e6faaee9a9cdb97755b8bfc34c762b21dd35e
Ensar Hamzic <ensarhamzic01@gmail.com>
2021-09-06 21:16:07 +0200

    Added signin in

--- server/dataValidation/usersValidation.js
+++ server/dataValidation/usersValidation.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const usersJoiSchema = Joi.object({
-  fullName: Joi.string().alphanum().required().max(30),
+  fullName: Joi.string().required().max(30),
   email: Joi.string().email().required(),
   username: Joi.string().alphanum().min(3).max(20),
   password: Joi.string().min(5).max(30).required(),
